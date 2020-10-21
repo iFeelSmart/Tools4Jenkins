af22a5234bc76d4a40fa3eb97c5b5b67df010039
Guillaume Stienon <guillaume.stienon@ifeelsmart.com>
2020-10-21 15:06:40 +0200

    fix: review update

--- src/api/job.groovy
+++ src/api/job.groovy
@@ -26,6 +26,9 @@ def start(){
 
                         env.M_JobName="${M_Ci.getJobName()}"
                         env.M_AwsS3Pipeline="${M_Ci.getPipelineDirName()}"
+                        if ( ( "${env.M_Hardware}" == "null" ) || ( "${env.M_Hardware}" == "" ) ){
+                            env.M_Hardware="${M_Ci.getJobName()}"
+                        }
                         if ( ( "${env.M_BranchName}" == "null" ) || ( "${env.M_BranchName}" == "" ) ){
                             env.M_BranchName="${env.BRANCH_NAME}"
                         }
